c98dc7509c0e8f8527f48eecfa4b6cbe50155c3e
FelipeLima <mota.felipelima@gmail.com>
2024-06-01 23:56:26 -0300

    Tipos Primitivos

---
+++ src/project/test/aula/introducao/TiposPrimitivos.java
@@ -0,0 +1,25 @@
+package project.test.aula.introducao;
+
+//psvm, para public static void main//
+//tipos primitivos: int, float, double, char, byte, boolean, short, long//
+
+import java.net.SocketOption;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int age = 18;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = 2000.0F;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean falso = false;
+        boolean verdadeiro = true;
+        char caractere = '\u0041';
+
+        System.out.println("A idade é: "+age+" anos.");
+        System.out.println("char " +caractere);
+        System.out.println(verdadeiro);
+
+    }
+}
