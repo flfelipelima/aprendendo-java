184ab634fd965592c1bc0e7efd7dca63e6ebfaf9
FelipeLima <mota.felipelima@gmail.com>
2024-06-02 00:52:03 -0300

    Exercício 1

---
+++ src/project/test/aula/introducao/exercicioTiposPrimitivos.java
@@ -0,0 +1,12 @@
+package project.test.aula.introducao;
+
+public class exercicioTiposPrimitivos {
+    public static void main(String[] args) {
+        String name = "João";
+        String address = "ABC 123";
+        double payment = 4600.00;
+        String datePayment = "02/06/2024";
+
+        System.out.println("Eu "+name+", morando no endereço "+address+", confirmo que recebi o salário de "+payment+", na data "+datePayment+".");
+    }
+}
